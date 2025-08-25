eb78ad376915e0100ed788205c5a8dc3dce52e54
Jeewani Sapkota <sapkotajiwani6@gmail.com>
2025-08-25 12:06:01 +0545

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# password_strength-validator
+Number Guessing Game
+
+A simple number guessing game built with HTML,  and JavaScript.
+
+This project was created purely for learning purposes and practicing DOM manipulation and event handling in JavaScript.
---
+++ script1.js
@@ -0,0 +1,20 @@
+const secretNumber = Math.floor(Math.random() * 10) + 1;
+ const checkBtn = document.getElementById('checkBtn');
+ const feedback = document.getElementById('feedback');
+
+checkBtn.addEventListener('click', ()=>{
+    const guess= +(document.getElementById('guess').value)
+    if(guess<1 || guess>=10 ||  isNaN(guess)){
+        feedback.textContent="please enter a valid number";
+    }
+    
+   else if(guess==secretNumber){
+        feedback.textContent="congrulations you guessed a number successfully";
+    }
+   else if(guess> secretNumber){
+        feedback.textContent="too high";
+    }
+    else{
+        feedback.textContent="too low";
+    }
+    });
